781b7961ec1fa5204a8ad51baeb3210d0c0eb552
nono96 <nohame.lemgadar@edu.ece.fr>
2018-12-22 16:21:14 +0100

    Version 1.0 Les tables se créent bien, aucuns messages d'erreures

--- src/model/DataAccess.java
+++ src/model/DataAccess.java
@@ -29,14 +29,14 @@ public class DataAccess implements AutoCloseable {
   public boolean initDataStore(int NumeroParticipant)
       throws DataAccessException {
     String sql = null;
-  
-    try {
+     try {
       // drop existing tables, if any
       Statement statement = connection.createStatement();
      try {
         statement.executeUpdate("DROP TABLE PARTICIPANT;");
          statement.executeUpdate("DROP TABLE QUESTIONNAIRE  ;");
          statement.executeUpdate("DROP TABLE REPPHOTOS;");
+         
          statement.executeUpdate("DROP TABLE DONNEESBCI;");
       } catch (SQLException e) {
         // likely cause: table does not exists: print error and go on
@@ -45,20 +45,16 @@ public class DataAccess implements AutoCloseable {
       } 
       // ...
       // create tables
-      sql = "CREATE TABLE PARTICIPANT (ID INT UNSIGNED PRIMARY KEY NOT NULL , IDREP INT  );";
+      sql = "CREATE TABLE PARTICIPANT (ID INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT, IDREP INT  );";
+     
       statement.executeUpdate(sql);
-      sql="INSERT INTO PARTICIPANT  VALUES("+1+" ,"+1+")";
-      statement.executeUpdate(sql);
-      
-     sql = "CREATE TABLE REPPHOTOS (IDREP INT PRIMARY KEY  NOT NULL ,  PASSAGE varchar(100) );";
-     statement.executeUpdate(sql);
-       sql="INSERT INTO REPPHOTOS("+1+", "+"Passage des images"+")";
+       sql = "CREATE TABLE REPPHOTOS (IDREP INT ,  Passage varchar(100) );";
      statement.executeUpdate(sql);
       sql = "CREATE TABLE QUESTIONNAIRE(ID INT UNSIGNED PRIMARY KEY  REFERENCES PARTICIPANT(ID), "
               + "AGE INT UNSIGNED NOT NULL, SEXE BOOLEAN NOT NULL, DESIR varchar(100));";
-  statement.executeUpdate(sql);
-  sql="CREATE TABLE DONNEESBCI (IDparticipant INT UNSIGNED PRIMARY KEY  REFERENCES PARTICIPANT(ID), CHEMIN varchar(100));";
-  statement.executeUpdate(sql);   
+      statement.executeUpdate(sql);
+  sql="CREATE TABLE DONNEESBCI (ID INT UNSIGNED PRIMARY KEY  REFERENCES PARTICIPANT(ID), CHEMIN varchar(100))";
+  statement.executeUpdate(sql); 
   // ...
       // populate tables if needed
       // ...
@@ -69,6 +65,53 @@ public class DataAccess implements AutoCloseable {
     }
     // TODO
   }
+  /*
+      try {
+      // drop existing tables, if any
+      Statement statement = connection.createStatement();
+     try {
+        statement.executeUpdate("DROP TABLE PARTICIPANT;");
+         statement.executeUpdate("DROP TABLE QUESTIONNAIRE  ;");
+         statement.executeUpdate("DROP TABLE REPPHOTOS;");
+         
+         statement.executeUpdate("DROP TABLE DONNEESBCI;");
+      } catch (SQLException e) {
+        // likely cause: table does not exists: print error and go on
+        System.err.print("drop table PARTICIPANT, Donnees bci, Questionnaire et Repphotos: " + e);
+        System.err.println(", going on...");
+      } 
+      // ...
+      // create tables
+      
+      sql = "create table participant(ID INT UNSIGNED PRIMARY KEY NOT NULL , IDREP INT  );";
+      statement.executeUpdate(sql);
+      sql="INSERT INTO PARTICIPANT  VALUES("+1+" ,"+1+")";
+      statement.executeUpdate(sql);
+    
+     sql = "create table repphotos(IDREP INT PRIMARY KEY  NOT NULL ,  PASSAGE varchar(100) );";
+     statement.executeUpdate(sql);
+       sql="insert into repphotos("+1+", "+"Passage des images"+")";
+     statement.executeUpdate(sql);
+     
+     sql = "insert into questionnaire(IDpart INT PRIMARY KEY NOT NULL,  AGE INT , SEXE BOOLEAN , DESIR varchar(100))";
+    //  sql = "create table questionnaire(IDpart INT  PRIMARY KEY REFERENCES participant(ID),AGE INT UNSIGNED NOT NULL, SEXE BOOLEAN NOT NULL, DESIR varchar(100));";
+      statement.executeUpdate(sql);
+            String hello = "Ce que j'aime ...";
+             sql = "INSERT INTO questionnaire (`ID`, `AGE`, `SEXE`, `DESIR`) VALUES ('1', '1', '0', 'hello');";
+             statement.executeUpdate(sql);
+  sql="CREATE TABLE DONNEESBCI (IDparticipant INT UNSIGNED PRIMARY KEY  REFERENCES PARTICIPANT(ID), CHEMIN varchar(100));";
+  statement.executeUpdate(sql);   
+  // ...
+      // populate tables if needed
+      // ...
+          return true;
+  
+    } catch (SQLException e) {
+      System.err.println("Problème dans la création des tables "+ ": " + e);
+      return false;
+    }
+    // TODO
+  }*/
  public boolean newParticipant(  boolean Sexe, int Age, String list_imag ,int idrep ) throws SQLException{
      String sql;
      Statement s = connection.createStatement();
@@ -84,7 +127,7 @@ public class DataAccess implements AutoCloseable {
      while(rs.next()){
             nb2 = rs.getInt(rs.getInt("ID"));
         //    if (nb1!=nb2){break;}
- }
+ }}
        //Maintenant on est sur d'avoir un ID unique non reconnaissable !
       System.out.println("le numero du nouveau participant est :"+nb1);
        //Partie 2 on insére dans notre BDD nb1 : id du participant :)
@@ -106,10 +149,10 @@ public class DataAccess implements AutoCloseable {
      return true;
    } 
  //Regardons d'abord si tout cela marche ! :)
- }
 
     @Override
     public void close() throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-}
+ }
+
